3b3ff68aae3ba2014458a0722b7ce7d3890ee518
troublemake3r <m.a.dukaczewska@gmail.com>
2024-12-10 19:33:34 +0100

    prefab do diamentu i niedziałająca funkcja do wykrywania kolizji

---
+++ TinyHeroes/Assets/Scripts/Collectibles/DiamondCollisionCheck.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class DiamondCollisionCheck : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            Destroy(gameObject);
+        }
+    }
+}
