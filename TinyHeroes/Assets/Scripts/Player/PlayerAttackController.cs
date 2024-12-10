f326144ceabcef8461fa4d652a8b3f6b965b1e26
Informejtycy-Dawka <dawkawody@wp.pl>
2024-12-10 18:20:39 +0100

    Dodano wstępny prototyp ataku

--- TinyHeroes/Assets/Scripts/Player/PlayerAttackController.cs
+++ TinyHeroes/Assets/Scripts/Player/PlayerAttackController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerInputHandler))]
 public class PlayerAttackController : MonoBehaviour
 {
+    [Header("Attack Settings")]
+    [SerializeField] private Transform _handPoint;
+    [SerializeField] private float _attackRange;
+    [SerializeField] private LayerMask _attackableLayers;
+
+    [Header("Parts to Ingore")]
+    [SerializeField] private Collider2D _bodyColl;
+
     private PlayerAnimationController _animationController;
     private PlayerInputHandler _inputHandler;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,6 +27,19 @@ public class PlayerAttackController : MonoBehaviour
         if (_inputHandler.Attack1WasPressed && !_animationController.IsInAirOrJumping())
         {
             _animationController.AnimateAttack(1);
+        }
+    }
+
+    // Invoked by an animation event
+    public void Attack1()
+    {
+        Vector2 attackPoint = new Vector2(_handPoint.position.x, _handPoint.position.y);
+        foreach (Collider2D hit in Physics2D.OverlapCircleAll(attackPoint, _attackRange))
+        {
+            if (hit != _bodyColl)
+            {
+                Debug.Log(hit);
+            }
         }
     }
 }
