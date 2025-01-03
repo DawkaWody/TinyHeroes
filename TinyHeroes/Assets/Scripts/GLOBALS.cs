15220f80aa9af8cd016607b6925651def672cbc1
DawkaWody <dawkawody@wp.pl>
2025-01-03 17:27:13 +0100

    Dodano platformę która zmienia (w teori) kolor gdy jest na niej 1 gracz przez określoną liczbę sekund (omg)

--- TinyHeroes/Assets/Scripts/GLOBALS.cs
+++ TinyHeroes/Assets/Scripts/GLOBALS.cs
@@ -3,10 +3,5 @@ using System.Collections.Generic;
 public class GLOBALS
 {
     public static string playerTag = "Player";
-    public static Dictionary<int, string> playerColors = new Dictionary<int, string>()
-    {
-        { 0, "blue" },
-        { 1, "pink" },
-        { 2, "white" }
-    };
+    public static List<string> playerColors = new List<string>() { "blue", "pink", "white" };
 }
--- TinyHeroes/Assets/Scripts/Gamemodes/KingOfThePlatforms/KotpPlatform.cs
+++ TinyHeroes/Assets/Scripts/Gamemodes/KingOfThePlatforms/KotpPlatform.cs
@@ -1,15 +1,50 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class KotpPlatform : MonoBehaviour
 {
     public string color;
+    public float _recolorTime;
+
+    private List<int> _playersOn = new();
+    private float _standingTimer;
+    private bool _capturing;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(GLOBALS.playerTag))
         {
-            color = GLOBALS.playerColors[other.GetComponentInParent<PlayerData>().index];
-            Debug.Log(color);
+            _playersOn.Add(other.GetComponentInParent<PlayerData>().index);
+            Debug.Log(other.gameObject.name);
         }
+    }
+
+    void Update()
+    {
+        if (_playersOn.Count == 1)
+        {
+            if (!_capturing)
+            {
+                _capturing = true;
+                _standingTimer = _recolorTime;
+            }
+            _standingTimer -= Time.deltaTime;
+
+            if (_standingTimer <= 0f)
+            {
+                color = GLOBALS.playerColors[_playersOn[0]];
+                Debug.Log(color);
+            }
+        }
+        else
+        {
+            _capturing = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(GLOBALS.playerTag))
+            _playersOn.Remove(other.GetComponentInParent<PlayerData>().index);
     }
 }
