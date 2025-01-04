eb9221c026125f2b09517c5756215f23434ba51c
DawkaWody <dawkawody@wp.pl>
2025-01-04 19:51:28 +0100

    Dodano King of the platorms (bez visuali)

--- TinyHeroes/Assets/Scripts/Gamemodes/KingOfThePlatforms/KotpManager.cs
+++ TinyHeroes/Assets/Scripts/Gamemodes/KingOfThePlatforms/KotpManager.cs
@@ -1,18 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class KotpManager : MonoBehaviour
 {
     [SerializeField] private KotpPlatform[] _platforms;
+    [SerializeField] private float _captureTime;
+    [SerializeField] private float _gameTime;
+
+    private float _gameTimer;
+    private bool _isPlaying;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        EnableAllPlatforms();
+
+        _gameTimer = _gameTime;
+        _isPlaying = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_isPlaying)
+        {
+            _gameTimer -= Time.deltaTime;
+
+            if (_gameTimer <= 0)
+            {
+                Debug.Log("Game has ended!!");
+                DisableAllPlatforms();
+                foreach (int playerIndex in GetWinners())
+                {
+                    Debug.Log("Player " + GLOBALS.playerColors[playerIndex] + " wins");
+                }
+
+                _isPlaying = false;
+            }
+        }
+    }
+
+    void EnableAllPlatforms()
+    {
+        foreach (KotpPlatform platform in _platforms)
+        {
+            platform.enabled = true;
+            platform.color = "";
+            platform.captureTime = _captureTime;
+        }
+    }
+    void DisableAllPlatforms()
+    {
+        foreach (KotpPlatform platform in _platforms)
+        {
+            platform.enabled = false;
+        }
+    }
+
+    List<int> GetWinners()
+    {
+        List<int> scores = new List<int>(new int[GLOBALS.playerColors.Count]);
+        foreach (KotpPlatform platform in _platforms)
+        {
+            if (platform.color == "") continue;
+
+            scores[GLOBALS.playerColors.IndexOf(platform.color)]++;
+        }
+
+        int maxScore = scores.Max();
+
+        return Enumerable.Range(0, scores.Count)
+            .Where(i => scores[i] == maxScore)
+            .ToList();
     }
 }
--- TinyHeroes/Assets/Scripts/Gamemodes/KingOfThePlatforms/KotpPlatform.cs
+++ TinyHeroes/Assets/Scripts/Gamemodes/KingOfThePlatforms/KotpPlatform.cs
@@ -1,24 +1,23 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class KotpPlatform : MonoBehaviour
 {
-    public string color;
-    public float _recolorTime;
+    [HideInInspector] public string color;
+    [HideInInspector] public float captureTime;
 
-    private List<int> _playersOn = new();
+    private List<int> _playersOn;
     private float _standingTimer;
     private bool _capturing;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        if (other.CompareTag(GLOBALS.playerTag))
-        {
-            _playersOn.Add(other.GetComponentInParent<PlayerData>().index);
-            Debug.Log(other.gameObject.name);
-        }
+        _playersOn = new List<int>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (_playersOn.Count == 1)
@@ -26,19 +25,34 @@ public class KotpPlatform : MonoBehaviour
             if (!_capturing)
             {
                 _capturing = true;
-                _standingTimer = _recolorTime;
+                _standingTimer = captureTime;
             }
             _standingTimer -= Time.deltaTime;
 
             if (_standingTimer <= 0f)
             {
                 color = GLOBALS.playerColors[_playersOn[0]];
-                Debug.Log(color);
+
+                if (color.Equals("pink"))
+                    Debug.Log("pink");
+                else if (color.Equals("blue"))
+                    Debug.Log("blue");
+                else if (color.Equals("white"))
+                    Debug.Log("white");
             }
         }
         else
         {
             _capturing = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(GLOBALS.playerTag))
+        {
+            _playersOn.Add(other.GetComponentInParent<PlayerData>().index);
+            Debug.Log(other.gameObject.name);
         }
     }
 
