f029514db28e0957acd04f6bbc677c159f6647c6
DawkaWody <dawkawody@wp.pl>
2025-02-02 17:42:08 +0100

    Dodano wykrywanie padów

--- TinyHeroes/Assets/Scripts/InputManager.cs
+++ TinyHeroes/Assets/Scripts/InputManager.cs
@@ -1,6 +1,8 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class DeviceManager : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,6 +13,9 @@ public class DeviceManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (InputSystem.devices.OfType<Gamepad>().Count() > 1)
+        {
+            Debug.Log("Multiple gamepads connected");
+        }
     }
 }
