1313b9ec28a96b1e11d0a75b08b1cc36ac2c8411
DawkaWody <dawkawody@wp.pl>
2025-03-04 20:16:10 +0100

    Dodano animacje do wszystkich kolorów graczy + baza pod local

--- TinyHeroes/Assets/Scripts/JoiningController.cs
+++ TinyHeroes/Assets/Scripts/JoiningController.cs
@@ -2,12 +2,15 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputManager : MonoBehaviour
+public class JoiningController : MonoBehaviour
 {
+
+    private PlayerInputManager _playerInputManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _playerInputManager = GetComponent<PlayerInputManager>();    
     }
 
     // Update is called once per frame
@@ -17,5 +20,10 @@ public class InputManager : MonoBehaviour
         {
             Debug.Log("Multiple gamepads connected");
         }
+    }
+
+    public void SetupPlayer(PlayerInput playerInput)
+    {
+        _playerInputManager.playerPrefab = this.gameObject;
     }
 }
